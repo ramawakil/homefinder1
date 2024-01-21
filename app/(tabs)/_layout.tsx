4cb25cfdc267aff867a740088f95b85d044923db
Ramadhan Marijani <ramadhanwakil19@gmail.com>
2024-01-21 23:12:30 +0300

    changed app name to homefinder1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,61 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Tabs } from 'expo-router';
+import {Ionicons, FontAwesome5, MaterialCommunityIcons} from '@expo/vector-icons';
+import Colors from "@/style/Colors";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+const Layout = () => {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors.primary,
+                tabBarLabelStyle: {
+                    fontFamily: 'mon-sb',
+                },
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    tabBarLabel: 'Explore',
+                    tabBarIcon: ({ size, color }) => <Ionicons name="search" size={size} color={color} />,
+                }}
+            />
+            {/*<Tabs.Screen*/}
+            {/*    name="whishlists"*/}
+            {/*    options={{*/}
+            {/*        tabBarLabel: 'Wishlists',*/}
+            {/*        tabBarIcon: ({ size, color }) => (*/}
+            {/*            <Ionicons name="heart-outline" size={size} color={color} />*/}
+            {/*        ),*/}
+            {/*    }}*/}
+            {/*/>*/}
+            {/*<Tabs.Screen*/}
+            {/*    name="trips"*/}
+            {/*    options={{*/}
+            {/*        tabBarLabel: 'Trips',*/}
+            {/*        tabBarIcon: ({ size, color }) => <FontAwesome5 name="airbnb" size={size} color={color} />,*/}
+            {/*    }}*/}
+            {/*/>*/}
+            {/*<Tabs.Screen*/}
+            {/*    name="inbox"*/}
+            {/*    options={{*/}
+            {/*        tabBarLabel: 'Inbox',*/}
+            {/*        tabBarIcon: ({ size, color }) => (*/}
+            {/*            <MaterialCommunityIcons name="message-outline" size={size} color={color} />*/}
+            {/*        ),*/}
+            {/*    }}*/}
+            {/*/>*/}
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    tabBarLabel: 'Profile',
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+                    headerShown: false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Ionicons name="person-circle-outline" size={size} color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+};
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+export default Layout;
